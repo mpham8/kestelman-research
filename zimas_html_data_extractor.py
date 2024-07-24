0e712bc1c28a112bf36ed9e45f0d2a507f4ec0fc
mpham8 <pham_michael@outlook.com>
2024-07-23 23:04:25 -0700

    final versions used

--- zimas_html_data_extractor.py
+++ zimas_html_data_extractor.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import numpy as np
+import csv
+
 
 
 
 html_files_directory = "zimas_html_downloads2/"
 html_data_directory = "zimas_html_data/"
-buildingpermits_data_path = "buildingpermits_data.csv"
+# buildingpermits_data_path = "buildingpermits_data.csv"
 
 #create new columns (one time)
 # buildingpermits_data_df["address"] = np.nan
@@ -30,12 +32,12 @@ buildingpermits_data_path = "buildingpermits_data.csv"
 # buildingpermits_data_df["entered_data"] = 0
 
 
-def get_pins_downloaded():
+def get_pins_downloaded(files_directory):
   """
-  gets a set of the pins of files already downloaded (pin files in prescribed folder)
+  gets a set of the pins of files already downloaded/processed (pin files in prescribed folder)
 
   parameters:
-  none
+  files_directory - which directory to check
 
   returns
   pins_downloaded_set (set) - set of all pins downloaded already
@@ -44,9 +46,9 @@ def get_pins_downloaded():
   pins_downloaded_set = set()
 
   # Check if the directory exists
-  if os.path.exists(html_files_directory):
+  if os.path.exists(files_directory):
     # Iterate over all files in the directory
-    for filename in os.listdir(html_files_directory):
+    for filename in os.listdir(files_directory):
     # Extract the pin from the filename and add it to the set
       pin, _ = os.path.splitext(filename)  # pin is the filename without the extension
       pins_downloaded_set.add(pin)
@@ -107,83 +109,69 @@ def find_multiple_text_for_label(label, rows):
 
 
 def main():
-  buildingpermits_data_df = pd.read_csv(buildingpermits_data_path)
-
-  counter = (buildingpermits_data_df['entered_data'] == 1).sum()
-  print(f"{counter} proccessed...")
-
-
 
   #get a set of the pins downloaded
-  pins_downloaded_set = get_pins_downloaded()
+  pins_downloaded_set = get_pins_downloaded(html_files_directory)
+  pins_proccessed_set = get_pins_downloaded(html_data_directory)
 
-  for index, row in buildingpermits_data_df.iterrows():
-    pin = row['PIN_NBR']
-    entered_data = row['entered_data']
+  total_pins = len(pins_downloaded_set)
 
+  for pin in pins_downloaded_set:
+    
+    pins_proccessed_set = get_pins_downloaded(html_data_directory)
     if pin not in pins_downloaded_set:
       continue
-
-    if entered_data == 1:
+    if pin in pins_proccessed_set:
        continue
+
 
     with open(html_files_directory + pin + ".html", 'r') as file:
       # Read the content of the file
       html_response = file.read()
     
 
-
-
     soup = BeautifulSoup(html_response, 'html.parser')
     rows = soup.find_all('tr')
-    
-    addresses = find_addresses(rows)
-    zip_code = find_text_for_label('ZIP Code', rows)
-    pin_number = find_text_for_label('PIN Number', rows)
-    assessor_parcel_no = find_text_for_label('Assessor Parcel No. (APN)', rows)
-    community_plan_area = find_text_for_label('Community Plan Area', rows)
-    area_planning_commission = find_text_for_label('Area Planning Commission', rows)
-    neighborhood_council = find_text_for_label('Neighborhood Council', rows)
-    census_tract = find_text_for_label('Census Tract #', rows)
-    zoning = find_text_for_label('Zoning', rows)
-    general_plan_land_use = find_text_for_label('General Plan Land Use', rows)
-    hillside_area = find_text_for_label('Hillside Area (Zoning Code)', rows)
-    apn_area = find_text_for_label('APN Area (Co. Public Works)*', rows)
-    rent_stabilization_ordinance = find_text_for_label('Rent Stabilization Ordinance (RSO)', rows)
-    ellis_act_property = find_text_for_label('Ellis Act Property', rows)
-    tenant_protection_act = find_text_for_label('AB 1482: Tenant Protection Act', rows)
-    environmental_cases = find_multiple_text_for_label('Environmental', rows)
-    city_planning_commission_cases = find_multiple_text_for_label('City Planning Commission', rows)
 
+    #create csv data structure
+    data = {
+        "pin": pin,
+        "addresses": find_addresses(rows),
+        "zip_code": find_text_for_label('ZIP Code', rows),
+        "pin_number": find_text_for_label('PIN Number', rows),
+        "assessor_parcel_no": find_text_for_label('Assessor Parcel No. (APN)', rows),
+        "community_plan_area": find_text_for_label('Community Plan Area', rows),
+        "area_planning_commission": find_text_for_label('Area Planning Commission', rows),
+        "neighborhood_council": find_text_for_label('Neighborhood Council', rows),
+        "census_tract": find_text_for_label('Census Tract #', rows),
+        "zoning": find_text_for_label('Zoning', rows),
+        "general_plan_land_use": find_text_for_label('General Plan Land Use', rows),
+        "hillside_area": find_text_for_label('Hillside Area (Zoning Code)', rows),
+        "apn_area": find_text_for_label('APN Area (Co. Public Works)*', rows),
+        "rent_stabilization_ordinance": find_text_for_label('Rent Stabilization Ordinance (RSO)', rows),
+        "ellis_act_property": find_text_for_label('Ellis Act Property', rows),
+        "tenant_protection_act": find_text_for_label('AB 1482: Tenant Protection Act', rows),
+        "environmental_cases": find_multiple_text_for_label('Environmental', rows),
+        "city_planning_commission_cases": find_multiple_text_for_label('City Planning Commission', rows)
+    }
 
-    buildingpermits_data_df.at[index, 'address'] = addresses
-    buildingpermits_data_df.at[index, 'zip_code'] = zip_code
-    buildingpermits_data_df.at[index, 'pin_number'] = pin_number
-    buildingpermits_data_df.at[index, 'assessor_parcel_no'] = assessor_parcel_no
-    buildingpermits_data_df.at[index, 'community_plan_area'] = community_plan_area
-    buildingpermits_data_df.at[index, 'area_planning_commission'] = area_planning_commission
-    buildingpermits_data_df.at[index, 'neighborhood_council'] = neighborhood_council
-    buildingpermits_data_df.at[index, 'census_tract'] = census_tract
-    buildingpermits_data_df.at[index, 'zoning'] = zoning
-    buildingpermits_data_df.at[index, 'general_plan_land_use'] = general_plan_land_use
-    buildingpermits_data_df.at[index, 'hillside_area'] = hillside_area
-    buildingpermits_data_df.at[index, 'apn_area'] = apn_area
-    buildingpermits_data_df.at[index, 'rent_stabilization_ordinance'] = rent_stabilization_ordinance
-    buildingpermits_data_df.at[index, 'ellis_act_property'] = ellis_act_property
-    buildingpermits_data_df.at[index, 'tenant_protection_act'] = tenant_protection_act
-    buildingpermits_data_df.at[index, 'environmental_cases'] = environmental_cases
-    buildingpermits_data_df.at[index, 'city_planning_commission_cases'] = city_planning_commission_cases
-    buildingpermits_data_df.at[index, 'entered_data'] = 1
+    csv_file_path = html_data_directory + pin + ".csv"
 
+    #export to csv
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data.keys())
+        writer.writeheader()
+        writer.writerow(data)
 
+    #CLI output
     print(f"apn {pin} processed...")
-    buildingpermits_data_df.to_csv("buildingpermits_data.csv")
-    counter += 1
+    pins_proccessed = len(pins_proccessed_set)
+    if pins_proccessed % 10 == 0:
+      print("*"*30)
+      print(f"{pins_proccessed}/{total_pins} proccessed...")
+      print("*"*30)
 
-    if counter % 10 == 0:
-      print("*"*30)
-      print(f"{counter} proccessed...")
-      print("*"*30)
+    pins_proccessed_set.add(pin)
 
 if __name__ == "__main__":
   main()
